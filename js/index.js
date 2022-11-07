fb0391daa9a4f061d918bbbf5c9db52671320e70
Lifelinexie <2313621769@qq.com>
2022-11-07 19:17:47 +0800

    Second commit

--- js/index.js
+++ js/index.js
@@ -1,129 +1,146 @@
+//json返回数据的code的状态码
+const GET_OK = 20011,POST_OK = 20021,PUT_OK = 20031,DELETE_OK = 20041,
+        GET_ERR = 20010,POST_ERR = 20020,PUT_ERR = 20030,DELETE_ERR = 20040;
+
 // 添加文本
 const postForm = function(){
 
     let text = document.querySelector('.form .text');
     let textData = text.value;
-    $.post("http://localhost:8080/ToDoList_war/ToDoList",
-        {"text": textData,"method":"ADD"},
-        function (data) {
-            // console.log("success" + data);
-        },
-        "json"
-    );
-};
-// 查询所有state为0 即所有未办事项
-const getTextDefault = function(){
-        let header = '<div class="text-item">';
-
-        let last = '<button class="button check"><i class="fa fa-check"></i></button>' +
-                        '<button class="button close"><i class="fa fa-close"></i></button>'+
-                    '</div>';
-        $.get("http://localhost:8080/ToDoList_war/ToDoList", {state:0},
-            function (data) {
-                
-                $(".text-show .text:first").empty();
-                for(let i=0;i < data.length;i++){
-                    let text = '<div class = "item-text">'+ data[i].text +'</div>';
-                    $(".text-show .text:first").append(header + text +last);
-                    blingUpData(i,data[i].id);
-                    blingDelete(i,data[i].id);
-                }
-            },
-            "json"
-        );
-        
-    };
-// 查询所有state为 1 即所有已完成事项
-const getText = function(){
-    let header = '<div class="text-item">';
-
-    let last = '<button class="button close"><i class="fa fa-close"></i></button>'+
-                '</div>';
-
-    $.get("http://localhost:8080/ToDoList_war/ToDoList", {state:1},
-        function (data) {
-            $(".text-show .text:last").empty();
-            for(let i=0;i < data.length;i++){
-                let text = '<div class = "item-text">'+ data[i].text +'</div>';
-                $(".text-show .text:last").append(header + text + last);
-                blingDelete(i,data[i].id);
+    $.post("http://localhost:8080/Test02_war_exploded/ToDoList",
+        {"text": textData},
+        function (Data) {
+            if(Data.code === GET_OK){
+                joinHtml(Data.data)
             }
         },
         "json"
     );
 };
+const joinHtml = function (data){
+    let header = '<div class="text-item">';
+    let last = '<button class="button check"><i class="fa fa-check"></i></button>' +
+        '<button class="button close"><i class="fa fa-close"></i></button>'+
+        '</div>';
+    let text = '<div class = "item-text">'+ data.text +'</div>';
+    $(".text-show .text:first").append(header + text +last);
+    blingUpData(data.id);
+    blingDelete(data.id);
+}
+const  joinColse = function (data){
+    let header = '<div class="text-item">';
+
+    let last = '<button class="button close"><i class="fa fa-close"></i></button>'+
+        '</div>';
+    let text = '<div class = "item-text">'+ data.text +'</div>';
+    $(".text-show .text:last").append(header + text + last);
+    blingDelete(data.id);
+}
+
+// 查询所有state为0 即所有未办事项
+const getTextDefault = function(){
+
+        $.get("http://localhost:8080/Test02_war_exploded/ToDoList/0",
+            function (Data) {
+                $(".text-show .text:first").empty();
+                for(let i=0;i < Data.data.length;i++) {
+                    joinHtml(Data.data[i])
+                }
+            },
+        );
+        
+    };
+// 查询所有state为 1 即所有已完成事项
+const getText = function(){
+
+    $.get("http://localhost:8080/Test02_war_exploded/ToDoList/1",
+        function (Data) {
+            $(".text-show .text:last").empty();
+            for(let i=0;i < Data.data.length;i++){
+                joinColse(Data.data[i])
+            }
+        },
+    );
+};
 // 更新state 函数 传入参数 id
 const upData = function(id){
-    $.post("http://localhost:8080/ToDoList_war/ToDoList",
-        {"id": id,"method":"PUT"},
-        function (data) {
-            // console.log("success" + data);
-        },
-        "json"
-    );
+    $.ajax({
+        url : "http://localhost:8080/Test02_war_exploded/ToDoList/"+id,
+        type : "PUT",
+        async: false,
+        success : function(result){
+
+        }
+        })
 };
 
 // 删除事项函数,传入参数 id
-const deleteData = function(id){
-    $.post("http://localhost:8080/ToDoList_war/ToDoList",
-    {"id": id,"method":"DELETE"},
-    function (data) {
-        // console.log("success" + data);
-    },
-    "json"
-);
+ const deleteData = function(id){
+    $.ajax({
+        url : "http://localhost:8080/Test02_war_exploded/ToDoList/"+id,
+        type : "DELETE",
+        async: false,
+        success : function(result){
+
+        },
+        })
 };
 
 // 给按钮绑定事件
-
-// 给state更新按钮绑定事件，传入num 表示第几个元素,id 表示主键
-const blingUpData = function(num,id){
+// 给state更新按钮绑定事件，传入id 表示主键
+const blingUpData = function(id){
     let checkBtn = $(".check");
-    checkBtn.eq(num).click(function(){
-        upData(id);
+    checkBtn.last().click(function(){
+        upData(id)
+        getTextDefault()
     });
 };
-// 给删除按钮绑定事件，传入num 表示第几个元素,id 表示主键
-const blingDelete = function(num,id){
-    let closeBtn = $(".close");
-    
-    closeBtn.eq(num).click(function(){
-        deleteData(id);
+// 给删除按钮绑定事件，传入id 表示主键
+const blingDelete = function(id){
+    let closeBtn = $(".show .text-item .close");
+    closeBtn.last().click(function(){
+            deleteData(id)
+            getTextDefault()
+            getText()
     });
 }
-
+// 未办事项和已完成事项的切换
+const unToDoList = function(){
+    // 导航栏
+    $(".item:last").removeClass("show");
+    $(".item:first").addClass("show");
+    // 显示框
+    $(".text-show .text:last").removeClass("show");
+    $(".text-show .text:first").addClass("show");
+    // 请求数据
+    getTextDefault();
+}
+const DoList = function(){
+    // 导航栏
+    $(".item:first").removeClass("show");
+    $(".item:last").addClass("show");
+    // 显示框
+    $(".text-show .text:first").removeClass("show");
+    $(".text-show .text:last").addClass("show");
+    // 请求数据
+    getText();
+}
+// js函数执行
 $(document).ready(function () {
 
-    // 按钮点击事件
+    getTextDefault();
+    getText();
+    // 添加按钮点击事件
     $(".form .button").click(function(){
         // 函数调用
         postForm()
-        getTextDefault();
     });
-   
     // 未办事项点击事件
     $(".item:first").click(function(){
-        
-        // 导航栏
-        $(".item:last").removeClass("show");
-        $(".item:first").addClass("show");
-        // 显示框
-        $(".text-show .text:last").removeClass("show");
-        $(".text-show .text:first").addClass("show");
-        // 请求数据
-        getTextDefault();
+        unToDoList();
     });
     // 已完成点击事件
     $(".item:last").click(function(){
-        
-        // 导航栏
-        $(".item:first").removeClass("show");
-        $(".item:last").addClass("show");
-        // 显示框
-        $(".text-show .text:first").removeClass("show");
-        $(".text-show .text:last").addClass("show");
-        // 请求数据
-        getText();
+        DoList();
     });
-
 });
